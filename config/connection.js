2b587224686423df874d3b5958cc72ecc7572f94
DaniBlaney <daniblaney@gmail.com>
2019-05-22 12:30:32 -0400

    export connection

--- config/connection.js
+++ config/connection.js
@@ -13,3 +13,15 @@ var connection = mysql.createConnection({
   database: "burger_db"
 });
 
+connection.connect(function(err) {
+  //If there is an error when connecting to the database, log the error to the console.
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  //If a database connection is established, log the database thread number.
+  console.log("connected as id " + connection.threadId);
+});
+
+//Export the connection properties so that we can use them in other files.
+module.exports = connection;
