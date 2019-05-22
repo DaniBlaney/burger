7eca0f6e515ee0028c64cf0b15b17124ee4c73cb
DaniBlaney <daniblaney@gmail.com>
2019-05-22 13:46:40 -0400

    create new burger

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var burger = require('../models/burger.js');
+
+//Setup Routes
+//------------------------------------------------
+
+// Index Redirect
+router.get('/', function (req, res)
+{
+  res.redirect('/');
+});
+
+// Index Page
+router.get('/', function (req, res)
+{
+  burger.selectAll(function(data)
+  {
+    var handlebarsObject = { burgers: data };
+    //console.log(hbsObject);
+    res.render('index', handlebarsObject);
+  });
+});
+
+// Create a New Burger
+router.post('/burger/create', function (req, res)
+{
+  burger.insertOne(req.body.burger_name, function()
+  {
+    res.redirect('/');
+  });
+});
