bfda64c770de157ddff94737b37f780aabd42ec5
DaniBlaney <daniblaney@gmail.com>
2019-05-22 14:19:24 -0400

    require dependencies

---
+++ server.js
@@ -0,0 +1,10 @@
+//Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+
+//Define port the server will be listening on.
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+
