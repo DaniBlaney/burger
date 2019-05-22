0a6864edd9f302264d3661fe09bb1041c1511d6d
DaniBlaney <daniblaney@gmail.com>
2019-05-22 12:24:20 -0400

    npm

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Say It Ain’t Cilantro Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Say It Ain’t Cilantro Burger", false);
 
-INSERT INTO burgers (burger_name, devoured) VALUES ("Poutine on the Ritz Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Poutine on the Ritz Burger", true);
 
 INSERT INTO burgers (burger_name, devoured) VALUES ("Chile Relleno-You-Didn’t Burger", true);
 
-INSERT INTO burgers (burger_name, devoured) VALUES ("The Human Polenta-pede Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Human Polenta-pede Burger", false);
